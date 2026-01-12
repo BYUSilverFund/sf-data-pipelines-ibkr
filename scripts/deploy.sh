840cca213a7b41172a1efc9ef0d956c20bfd1663
Ben Haggard <21bhaggard@gmail.com>
2026-01-12 15:38:51 -0700

    CodePipeline scripts and yaml files

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 1. Navigate to your Airflow directory on the EC2
+TARGET_DIR="/home/ec2-user/airflow"
+cd $TARGET_DIR
+
+# 2. Pull the latest code from GitHub
+git pull origin main
+
+# 3. Restart the containers with the new code
+# --build ensures that if you changed a Dockerfile, it builds it locally
+# -d runs it in the background
+docker-compose down
+docker-compose up --build -d
+
+# 4. Optional: Clean up old Docker images to save disk space
+docker image prune -f
